6ab402ef23fad9b810afd998a8ac2a062c7c6b64
Douglas Kazumi Lopes Ugochi <dl585@cornell.edu>
2013-10-08 17:10:41 -0400

    Started working on initial context

--- src/main/program/CubeXProgram.java
+++ src/main/program/CubeXProgram.java
@@ -1,7 +1,11 @@
 package main.program;
+import java.awt.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import main.context.*;
+import main.type.CubeXType;
+import main.type.CubeXTypeVariable;
 
 public class CubeXProgram {
 	
@@ -43,7 +47,22 @@ public class CubeXProgram {
 		GlobalContexts.variableContext=new VariableContext(null);
 		
 		//TODO finish this
+		ArrayList<CubeXClass> classes = new ArrayList<CubeXClass>();
+		ArrayList<CubeXFunctionDeclaration> functions = new ArrayList<CubeXFunctionDeclaration>();
+		CubeXClass newClass;
+		CubeXFunctionDeclaration newFunction;
+		CubeXScheme newScheme;
 		
+		//Iterable
+		newClass = new CubeXClass("Iterable", new ArrayList<CubeXTypeVariable>(Arrays.asList(new CubeXTypeVariable("E"))), null, CubeXType.getThing(), null, null, null);
+		classes.add(newClass);
+		
+		newScheme = new CubeXScheme(null, null, CubeXType.getBoolean());
+		newFunction = new CubeXFunctionDeclaration("negate",newScheme);
+		functions.add(newFunction);
+		
+		newClass = new CubeXClass("Boolean", null, null, CubeXType.getThing(), null, null, null);
+		classes.add(newClass);
 		
 	}
 	
