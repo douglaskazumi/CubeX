08f87aa5fbc576fc6437cda1b29b22c7a9a1c878
hibobyoyo <pgb55@cornell.edu>
2013-10-26 14:55:55 -0400

    started implimenting iterables

--- src/main/c/cubex_lib.c
+++ src/main/c/cubex_lib.c
@@ -9,4 +9,92 @@ void gc(Object target){
 }
 
 
+bool iterableHasNext(Object *obj)
+{
+	if(obj->numFields!=-2)
+	{
+		return (Object *)0xDEADBEEF; //shouldn't happen
+	}
 
+	iterable_t *iter = (iterable_t *)obj;
+
+	switch (iter->type)
+	{
+		case OBJECT:
+			finiteGeneralIterable_t *actualIter = (finiteGeneralIterable_t *)iter;
+			if(actualIter->index >= actualIter->numEntries)
+				return false;
+			return true;
+		break;
+		case INTEGER:
+			if(iter->isFinite)
+			{
+				finiteIntegerIterable_t *actualIter = (finiteIntegerIterable_t *)iter;
+				if(actualIter->current > actualIter->last)
+					return false;
+				return true;
+			}
+			else
+			{
+				return true;
+			}
+		break;
+		case BOOLEAN:
+
+			if(iter->isFinite)
+			{
+				finiteBooleanIterable_t *actualIter = (finiteBooleanIterable_t *)iter;
+				if(actualIter->current > actualIter->last)
+					return false;
+				return true;
+			}
+			else
+			{
+				infiniteBooleanIterable_t *actualIter = (infiniteBooleanIterable_t *)iter;
+				if(actualIter->current > true) // >1
+					return false;
+				return true;
+			}
+
+		break;
+	}
+
+}
+
+
+Object * iterableNext(Object * iter)
+{
+
+	//Assumes iter has a next item to return
+
+	if(obj->numFields!=-2)
+	{
+		return (Object *)0xDEADBEEF; //shouldn't happen
+	}
+
+	iterable_t *iter = (iterable_t *)obj;
+
+	switch (iter->type)
+	{
+		case OBJECT:
+			finiteGeneralIterable_t *actualIter = (finiteGeneralIterable_t *)iter;
+			return (actualIter->array)[actualIter->index];
+		break;
+		case INTEGER:
+				finiteIntegerIterable_t *actualIter = (finiteIntegerIterable_t *)iter;
+				int cur=(actualIter->current)++;
+
+				//TODO : create int object here
+				//return createInteger(cur);
+				return NULL;
+		break;
+		case BOOLEAN:
+			finiteBooleanIterable_t *actualIter = (finiteBooleanIterable_t *)iter;
+			bool cur=(actualIter->current)++;
+
+			//TODO : create int object here
+			//return createInteger(cur);
+			return NULL;
+		break;
+	}
+}
--- src/main/c/cubex_lib.h
+++ src/main/c/cubex_lib.h
@@ -1,13 +1,97 @@
 #ifndef _CUBEX_LIB
 #define _CUBEX_LIB
 
+#define true 1
+#define false 0
+#define NULL 0
+
+typedef char bool;
+
 typedef struct {
 	//vTable
 	int refCount;
 	int numFields;
-} Object;
+} object;
 
-Object newObject(struct type, void *params);
-void gc(Object target);
+/* ITERABLE SECTION*/
+
+typedef enum {INTEGER, BOOLEAN, OBJECT} iterabletype_t
+
+typedef struct {
+	//vTable
+	unsigned int refCount;
+	int numFields;
+	iterabletype_t type;
+	bool isFinite;
+} iterable_t;
+
+typedef struct {
+	//vTable
+	unsigned int refCount;
+	int numFields;
+
+	iterabletype_t type;
+	bool isFinite;
+
+	unsigned int numEntries;
+	Object **array;
+	unsigned int index;
+} finiteGeneralIterable_t;
+
+typedef struct {
+	//vTable
+	unsigned int refCount;
+	int numFields;
+
+	iterabletype_t type;
+	bool isFinite;
+
+	signed int current;
+	signed int last;
+
+} finiteIntegerIterable_t;
+
+typedef struct {
+	//vTable
+	unsigned int refCount;
+	int numFields;
+
+	iterabletype_t type;
+	bool isFinite;
+
+	bool current;
+	bool last;
+
+} finiteBooleanIterable_t;
+
+typedef struct {
+	//vTable
+	unsigned int refCount;
+	int numFields;
+
+	iterabletype_t type;
+	bool isFinite;
+
+	signed int current;
+
+} infiniteIntegerIterable_t;
+
+typedef struct {
+	//vTable
+	unsigned int refCount;
+	int numFields;
+
+	iterabletype_t type;
+	bool isFinite;
+
+	bool current;
+
+} infiniteBooleanIterable_t;
+
+
+Object * newObject(struct type, void *params);
+void gc(Object *target);
+bool iterableHasNext(Object * iter);
+Object * iterableNext(Object * iter);
 
 #endif
