1aba7db63614a9e10f384b4fa03007fdff767174
Douglas Kazumi Lopes Ugochi <dl585@cornell.edu>
2013-12-06 16:31:31 -0500

    Revert "Test2"
    This reverts commit 293a439f3ded19f95cb5668c8fc38e4f37d0f812.

--- src/main/util/CubeXCompiler.java
+++ src/main/util/CubeXCompiler.java
@@ -22,7 +22,7 @@ import org.antlr.v4.runtime.dfa.DFA;
 
 public class CubeXCompiler {
 	
-	public static boolean debug=true;
+	public static boolean debug=false;
 	
 public static void main(String[] args) throws IOException
 {
